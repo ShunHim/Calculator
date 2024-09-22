6dc1a41cf6bd7c4eb23f7927b8f4aa5e5b7d91aa
alice.ideadrive@gmail.com <alice.ideadrive@gmail.com>
2024-09-23 02:38:03 +0900

    (Fix) レイアウトモック作成 ビューモデルの表示部作成

--- WinForm/Components/CalculatorVM.cs
+++ WinForm/Components/CalculatorVM.cs
@@ -1,13 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WinForm.Components {
+﻿namespace WinForm.Components {
     /// <summary>
     /// ViewModel
     /// </summary>
     public class CalculatorVM {
+        /// <summary>
+        /// テーマモデル
+        /// </summary>
+        public Theme Theme { get; set; } = new(ThemeType.Dark);
+        /// <summary>
+        /// 数式表示
+        /// </summary>
+        public string TxFomula = "0";
+        /// <summary>
+        /// 入力・結果表示
+        /// </summary>
+        public string TxInput = "0";
+        /// <summary>
+        /// 格納数値
+        /// </summary>
+        public decimal NumInput = 0;
+        /// <summary>
+        /// 入力数値
+        /// </summary>
+        public decimal NumTemp = 0;
     }
 }
---
+++ WinForm/Components/Theme.cs
@@ -0,0 +1,45 @@
+﻿namespace WinForm.Components {
+    public class Theme {
+        public ThemeType Type { get; set; }
+        public string Bg { get; set; } = string.Empty;
+        public string Tx { get; set; } = string.Empty;
+        public string Btn1 { get; set; } = string.Empty;
+        public string Btn2 { get; set; } = string.Empty;
+        public string Btn3 { get; set; } = string.Empty;
+        public Theme(ThemeType type) {
+            switch (type) {
+                case ThemeType.Dark:
+                    SetDark();
+                    break;
+                case ThemeType.Light:
+                    SetLight();
+                    break;
+                default:
+                    break;
+            }
+            Type = type;
+        }
+        /// <summary>
+        /// ライトテーマ
+        /// </summary>
+        public void SetLight() {
+            Type = ThemeType.Light;
+            Bg = "bg-white";
+            Tx = "text-dark";
+            Btn1 = "btn-secondary btn-main-light";
+            Btn2 = "btn-secondary btn-sub-light";
+            Btn3 = "btn-primary";
+        }
+        /// <summary>
+        /// ダークテーマ
+        /// </summary>
+        public void SetDark() {
+            Type = ThemeType.Dark;
+            Bg = "bg-dark";
+            Tx = "text-light";
+            Btn1 = "btn-secondary btn-main-dark";
+            Btn2 = "btn-secondary btn-sub-dark";
+            Btn3 = "btn-success";
+        }
+    }
+}
